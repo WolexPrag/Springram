3f3d96dc5f7c762a5740be6b9af4dc03c4d5be24
Wolex <wolexprag@gmail.com>
2026-04-29 16:56:57 +0300

    feat(MVP): add Presenter,View

---
+++ Assets/UI/Presenter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using R3;
+
+public class Presenter
+{
+    private List<Command> _commands = new();
+    private CompositeDisposable _disposable;
+    protected View _view;
+    public void Init(View view)
+    {
+        _view = view;
+        _view.OnPlayClick.Subscribe(Play).AddTo(_disposable);
+    }
+    private void Play(Unit unit)
+    {
+        for (int i = 0; i < _commands.Count; i++)
+        {
+            _commands[i].TryExecute();
+        }
+    }
+}
---
+++ Assets/UI/View.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using R3;
+public class View : MonoBehaviour
+{
+    [SerializeField] private Button _play;
+    public Observable<Unit> OnPlayClick { get; private set; }
+
+    public void Awake()
+    {
+       OnPlayClick = _play.OnClickAsObservable();
+    }
+
+
+}
