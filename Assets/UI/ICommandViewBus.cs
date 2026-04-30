4896081d48f5002c250fde1405cb86dc5264e5f0
Wolex <wolexprag@gmail.com>
2026-04-30 23:25:48 +0300

    feat(ICommandViewBus): add interface and enum EventStateType

---
+++ Assets/UI/ICommandViewBus.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine.EventSystems;
+
+public interface ICommandViewBus
+{
+    public void OnNextDrag((CommandView, EventStateType, PointerEventData) _);
+    public void OnNextDrop((CommandView, PointerEventData) _);
+}
+
+
+public enum EventStateType
+{
+    Begin,
+    Perform,
+    End,
+}
