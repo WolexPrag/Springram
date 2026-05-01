3d3fcfbdba1669f4d8593b4be10c4d8809d97e01
Wolex <wolexprag@gmail.com>
2026-05-01 15:15:16 +0300

    fix(CommandView): fix OnDrop

--- Assets/UI/CommandView.cs
+++ Assets/UI/CommandView.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+[RequireComponent(typeof(CanvasGroup), typeof(RectTransform))]
 public class CommandView : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler, IDropHandler
 {
     private RectTransform _rect;
+    private CanvasGroup _canvasGroup;
     public Command Command { get; private set; }
     public void Awake()
     {
         _rect = GetComponent<RectTransform>();
+        _canvasGroup = GetComponent<CanvasGroup>();
     }
 
     public void Bind(Command command)
@@ -24,9 +27,23 @@ public class CommandView : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndD
     {
         _bus = bus;
     }
-    public void OnDrag(PointerEventData eventData) => _bus?.OnNextDrag((this, EventStateType.Begin, eventData));
-    public void OnBeginDrag(PointerEventData eventData) => _bus?.OnNextDrag((this, EventStateType.Perform, eventData));
-    public void OnEndDrag(PointerEventData eventData) => _bus?.OnNextDrag((this, EventStateType.End, eventData));
-    public void OnDrop(PointerEventData eventData) => _bus?.OnNextDrop((this, eventData));
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _canvasGroup.blocksRaycasts = false;
+        _bus?.OnNextDrag((this, EventStateType.Begin, eventData));
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        _bus?.OnNextDrag((this, EventStateType.Perform, eventData));
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        _bus?.OnNextDrag((this, EventStateType.End, eventData));
+        _canvasGroup.blocksRaycasts = true;
+    }
+    public void OnDrop(PointerEventData eventData)
+    {
+        _bus?.OnNextDrop((this, eventData));
+    }
 }
 
