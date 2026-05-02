997da7b2c82fa7053a6e67dcd636908dba2a961a
Wolex <wolexprag@gmail.com>
2026-05-02 12:44:47 +0300

    feat(UIEventContext): add struct with target, delta

---
+++ Assets/UI/UIEventContext.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+public struct UIEventContext<T>
+{
+    public T Target { get; private set; }
+    public Vector2 Delta { get; private set; }
+    public UIEventContext(T target,PointerEventData evenData)
+    {
+        Target = target;
+        Delta = evenData.delta;
+    }
+}
+
