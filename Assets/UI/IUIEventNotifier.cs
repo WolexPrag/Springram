92f6fce219d240c491e4e5859891101b86f676ef
Wolex <wolexprag@gmail.com>
2026-05-02 12:45:24 +0300

    feat(IUIEventNotifier): add interface

---
+++ Assets/UI/IUIEventNotifier.cs
@@ -0,0 +1,10 @@
+﻿using R3;
+
+public interface IUIEventNotifier<T>
+{
+    public Observable<UIEventContext<T>> OnBeginDrag { get; }
+    public Observable<UIEventContext<T>> OnPerformDrag { get; }
+    public Observable<UIEventContext<T>> OnEndDrag { get; }
+    public Observable<UIEventContext<T>> OnDrop { get; }
+}
+
