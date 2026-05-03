64b191483e5118a0fa1da31663a264b19736c6d5
Wolex <wolexprag@gmail.com>
2026-05-03 16:40:24 +0300

    feat(BlockMediator): add class

---
+++ Assets/UI/IDE/VisualIDE/BlockMediator.cs
@@ -0,0 +1,48 @@
+﻿using R3;
+public class BlockMediator
+{
+    protected IUIEventNotifier<Block> _notifier;
+    protected ITransition<Block> _transition;
+    protected IBlockContainer _container;
+    private CompositeDisposable _disposable = new();
+    public BlockMediator(IUIEventNotifier<Block> notifier, ITransition<Block> transition, IBlockContainer container)
+    {
+        _notifier = notifier;
+        _transition = transition;
+        _container = container;
+        notifier.OnBeginDrag.Subscribe(BeginDrag).AddTo(_disposable);
+        notifier.OnPerformDrag.Subscribe(PerformDrag).AddTo(_disposable);
+        notifier.OnEndDrag.Subscribe(EndDrag).AddTo(_disposable);
+        notifier.OnDrop.Subscribe(Drop).AddTo(_disposable);
+    }
+    public virtual void DeInit()
+    {
+        _disposable?.Dispose();
+    }
+    protected virtual void BeginDrag(UIEventContext<Block> context)
+    {
+        context.Target.SetInteractable(false);
+        int id = _container.GetIndex(context.Target);
+        _container.Remove(id);
+        _transition.Place(this, context.Target);
+    }
+    protected virtual void PerformDrag(UIEventContext<Block> context)
+    {
+        context.Target.MoveDelta(context.Delta);
+    }
+    protected virtual void EndDrag(UIEventContext<Block> context)
+    {
+        context.Target.SetInteractable(true);
+        _transition.Abort(this);
+    }
+    protected virtual void Drop(UIEventContext<Block> context)
+    {
+        int id = _container.GetIndex(context.Target);
+        Block taked = _transition.Take(this);
+        _container.Insert(id, taked);
+    }
+    public void Abort(Block block, int sibling)
+    {
+        _container.Insert(sibling, block);
+    }
+}
