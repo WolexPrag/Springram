a3de63c781f891116eaebdd0c23a9ebb10c1b8d0
Wolex <wolexprag@gmail.com>
2026-05-03 16:40:59 +0300

    feat(RegistryMediator): add class implementation BlockMediator

---
+++ Assets/UI/IDE/VisualIDE/RegistryMediator.cs
@@ -0,0 +1,29 @@
+﻿using R3;
+using System;
+using UnityEngine;
+class RegistryMediator : BlockMediator
+{
+    public RegistryMediator(IUIEventNotifier<Block> notifier, ITransition<Block> transition, IBlockContainer container) : base(notifier, transition, container)
+    {
+    }
+
+    protected override void BeginDrag(UIEventContext<Block> context)
+    {
+        int sibling = context.Target.transform.GetSiblingIndex();
+        _container.Insert(sibling,GameObject.Instantiate(context.Target));
+        base.BeginDrag(context);
+    }
+    protected override void PerformDrag(UIEventContext<Block> context)
+    {
+        base.PerformDrag(context);
+    }
+    protected override void EndDrag(UIEventContext<Block> context)
+    {
+        GameObject.Destroy(context.Target.gameObject);
+    }
+    protected override void Drop(UIEventContext<Block> context)
+    {
+        GameObject.Destroy(_transition.Take(this).gameObject);
+    }
+}
+
