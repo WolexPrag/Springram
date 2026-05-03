2db5fb44bd0e1f748b8be8303183e670d40601c6
Wolex <wolexprag@gmail.com>
2026-05-03 16:02:25 +0300

    feat(VisualIDEView): implement ITransition

--- Assets/UI/IDE/VisualIDE/VisualIDEView.cs
+++ Assets/UI/IDE/VisualIDE/VisualIDEView.cs
@@ -3,10 +3,13 @@ using System.Linq;
 using UnityEngine.EventSystems;
 using R3;
 
-public class VisualIDEView : IDEView
+public class VisualIDEView : IDEView , ITransition<Block>
 {
+    [SerializeField] private Transform _canvas;
     [SerializeField] private BlockPanel _registryPanel;
     [SerializeField] private BlockPanel _inputPanel;
+    [SerializeField] private int _lastSibling;
+    [SerializeField] private Block _holdItem;
 
     [SerializeField] private int _lastSibling = -1;
     [SerializeField] private Block _heldItem;
@@ -24,14 +27,25 @@ public class VisualIDEView : IDEView
     {
         _disposable?.Dispose();
     }
-    private void Return(Block view, BlockPanel scrollView)
+    public void Place(object source, Block item)
     {
-        scrollView.Insert(_lastSibling, view);
+        _holdItem = item;
+        _lastSibling = _holdItem.transform.GetSiblingIndex();
+        _holdItem.transform.SetParent(_canvas);
     }
-    public Block CreateNew()
-    {
-        return Instantiate(_heldItem);
 
+    public Block Take(object source)
+    {
+        _lastSibling = -1;
+        Block ret = _holdItem;
+        _holdItem = null;
+        return ret;
+    }
+    public void Abort(object source)
+    {
+        if (_holdItem==null) return; 
+        _holdItem.transform.SetSiblingIndex(_lastSibling);
+        _holdItem = null;
     }
 
     public override Command[] GetInput()
